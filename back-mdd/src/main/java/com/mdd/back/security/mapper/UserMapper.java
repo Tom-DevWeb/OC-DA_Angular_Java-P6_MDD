c16689a49fe0d2b3e5bdf28cdce5c3b7d052575b
Tom ANDRE <tom.andre@soprasteria.com>
2024-12-11 11:43:13 +0100

    feat(STEP-1): Fonctionnement de la route /users/register

--- back-mdd/src/main/java/com/mdd/back/security/mapper/UserMapper.java
+++ back-mdd/src/main/java/com/mdd/back/security/mapper/UserMapper.java
@@ -3,15 +3,9 @@ package com.mdd.back.security.mapper;
 import com.mdd.back.dto.requests.RegisterRequestDto;
 import com.mdd.back.entities.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
-import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
-
-@Mapper(componentModel = SPRING)
+@Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    UserMapper USER_MAPPER = Mappers.getMapper(UserMapper.class);
-
-
-    User UserRequestDtoToUser(RegisterRequestDto userDto);
+    User toUser(RegisterRequestDto userDto);
 }
--- back-mdd/src/main/java/com/mdd/back/services/UserService.java
+++ back-mdd/src/main/java/com/mdd/back/services/UserService.java
@@ -3,30 +3,30 @@ package com.mdd.back.services;
 import com.mdd.back.dto.requests.RegisterRequestDto;
 import com.mdd.back.entities.User;
 import com.mdd.back.repositories.UserRepository;
+import com.mdd.back.security.mapper.UserMapper;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import static com.mdd.back.security.mapper.UserMapper.USER_MAPPER;
 
 @Service
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserMapper userMapper;
 
 
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, UserMapper userMapper) {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.userMapper = userMapper;
     }
 
     public void register(RegisterRequestDto registerRequestDto) {
         registerRequestDto.setPassword(bCryptPasswordEncoder.encode(registerRequestDto.getPassword()));
-
-        User user = USER_MAPPER.UserRequestDtoToUser(registerRequestDto);
-
+        User user = userMapper.toUser(registerRequestDto);
         userRepository.save(user);
     }
 
