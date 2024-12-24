339e8e16599039cd3c8d426751c7bf8438388983
tandre <tom.andre@soprasteria.com>
2024-12-24 15:48:57 +0100

    feat(STEP-2): Ajout model BearerResponse

--- front-mdd/src/app/core/auth/auth.service.ts
+++ front-mdd/src/app/core/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpResponse} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {BearerResponse} from './models/bearerResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,9 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-  private baseUrl: string = "/users"
+  private baseUrl: string = "/auth"
 
-  login(email: string, password: string): Observable<HttpResponse<any>> {
-    return this.http.post<HttpResponse<any>>(`${this.baseUrl}/login`, {email, password})
+  login(email: string, password: string): Observable<HttpResponse<BearerResponse>> {
+    return this.http.post<HttpResponse<BearerResponse>>(`${this.baseUrl}/login`, {email, password})
   }
 }
