00a4c0c614f8fdf74dd455caaaf1f6e25d2f8f21
tandre <tom.andre@soprasteria.com>
2025-01-23 17:26:52 +0100

    chore: Ajout JavaDoc

--- back-mdd/src/main/java/com/mdd/back/entities/ThemeSubscription.java
+++ back-mdd/src/main/java/com/mdd/back/entities/ThemeSubscription.java
@@ -8,6 +8,10 @@ import lombok.Data;
 @Table(name = "theme_subscription")
 public class ThemeSubscription {
 
+    /**
+     * Clé primaire composée, définie dans la classe {@link ThemeSubscriptionId}.
+     * L'annotation {@code @EmbeddedId} indique que cette clé regroupe plusieurs colonnes.
+     */
     @EmbeddedId
     private ThemeSubscriptionId id;
 
